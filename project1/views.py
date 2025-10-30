4b22f9756183ab22d392a7f310b965cf887f88c3
Shahryar-Sohail <shahryar.ch123@gmail.com>
2025-10-30 23:16:47 +0500

    new app created

--- project1/views.py
+++ project1/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def home(request):
-    return HttpResponse("Welcome to Project 1 Home Page")
+    return render(request, "website\index.html")
 
 def about(request):
     return HttpResponse("Welcome to Project 1 About Page")
