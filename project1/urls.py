c94b6bcd698d53a84b710d6ee56e26110c8a25b2
Shahryar-Sohail <shahryar.ch123@gmail.com>
2025-11-07 23:58:44 +0500

    Super User Created

--- project1/urls.py
+++ project1/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -25,4 +25,7 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("project2/", include("project2.urls")),
+
+    
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
