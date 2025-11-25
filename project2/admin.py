7e95b697e94c7907b4a4fb39c218b0df6843a95d
Shahryar-Sohail <shahryar.ch123@gmail.com>
2025-11-26 00:16:46 +0500

    Working on Django models

--- project2/admin.py
+++ project2/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
-from .models import ProjectDetail
+from .models import ProjectDetail, ProjectReview, ProjectShop, ProjectCertificate
+
 
 # Register your models here.
-admin.site.register(ProjectDetail)
+class ProjectReviewInline(admin.TabularInline):
+    model = ProjectReview
+    extra = 2
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "type", "cost", "start_date", "end_date")
+    search_fields = ("title", "description")
+    list_filter = ("type", "start_date")
+    inlines = [ProjectReviewInline]
+
+
+class ProjectShopAdmin(admin.ModelAdmin):
+    list_display = ("name", "address")
+    search_fields = ("name", "description", "address")
+    # filter_horizontal = ("ProjectDetail",)
+
+
+class ProjectCertificateAdmin(admin.ModelAdmin):
+    list_display = ("project", "certificate_number", "issued_date", "valid_until")
+    search_fields = ("certificate_number", "project__title")
+
+
+admin.site.register(ProjectDetail, ProjectAdmin)
+admin.site.register(ProjectShop, ProjectShopAdmin)
+admin.site.register(ProjectCertificate, ProjectCertificateAdmin)
--- project2/models.py
+++ project2/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ProjectDetail(models.Model):
@@ -14,3 +15,38 @@ class ProjectDetail(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+
+# one to many
+class ProjectReview(models.Model):
+    project = models.ForeignKey(ProjectDetail, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Review by {self.user.username} for {self.project.title} - Rating: {self.rating}"
+    
+# many to many
+class ProjectShop(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    address = models.CharField(max_length=50)
+
+    project = models.ManyToManyField(ProjectDetail, related_name='shops')
+    
+
+    def __str__(self):
+        return self.name
+    
+# one to one
+class ProjectCertificate(models.Model):
+    project = models.OneToOneField(ProjectDetail, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issued_date = models.DateField(default=timezone.now)
+    valid_until = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Certificate for {self.project.title}: {self.certificate_name}"
